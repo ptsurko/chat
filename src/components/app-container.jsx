969a1ce5c4256a2f2c4bbc7a09d38f17ab53d09d
Pavel Tsurko <ptsurko@mz.com>
2017-07-20 16:02:11 -0700

    Add Sign up and Go to Github buttons

--- src/components/app-container.jsx
+++ src/components/app-container.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Types from 'prop-types';
 import satoriLogo from '../images/SatoriLogoWithText.svg';
+import githubLogo from '../images/github.svg';
 
 class AppContainer extends React.Component {
   render() {
@@ -8,6 +9,13 @@ class AppContainer extends React.Component {
       <div className="appContainer">
         <div className="headerBar">
           <img src={satoriLogo} className="headerBar-logo" alt="Satori Logo" />
+          <nav className="headerBar-nav">
+            <a className="button-github" href="https://github.com/satori-com/chat">
+              Go to GitHub
+              <img className="github-icon" src={githubLogo} alt="GitHub" />
+            </a>
+            <a className="button-signup" href="https://developer.satori.com/#/signup">Sign up</a>
+          </nav>
         </div>
         {this.props.children}
       </div>
